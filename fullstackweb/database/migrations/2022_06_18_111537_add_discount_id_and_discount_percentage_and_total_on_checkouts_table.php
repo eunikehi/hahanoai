98633e21a09da9ac22d38e909c399432f783437e
eunikehi <eunikehi77@gmail.com>
2022-06-18 18:17:20 +0700

    migration user, camp, camp_benefits, checkout, discount

---
+++ fullstackweb/database/migrations/2022_06_18_111537_add_discount_id_and_discount_percentage_and_total_on_checkouts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\ForeignKeyDefinition;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('checkouts', function (Blueprint $table) {
+            $table->foreignId('discount_id')->nullable();
+            $table->unsignedInteger('discount_percentage')->nullable();
+            $table->unsignedInteger('total')->default(0);
+
+            $table->foreign('discount_Id')->references('id')->on('discounts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('checkouts', function (Blueprint $table) {
+            $table->dropForeign('checkouts_discount_id_foreign');
+            $table->dropColumn(['discount_id', 'discount_percentage', 'total']);
+        });
+    }
+};
